7736cc7d34097345c4c41d5ffb59594b1eabcc15
unknown <140576667+Dungnope@users.noreply.github.com>
2026-09-14 21:52:54 +0700

    use setState with react class and merge state(which data need update, which keep)

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -12,18 +12,25 @@ class MyComponent extends React.Component {
     }
     
     handleClick(event){
-        console.log("click me");
-    }
+        // console.log("My name is ", this.state.name);
 
+        //merge State => react class have this
+        this.setState({
+            name: 'itDev',
+            age: Math.floor((Math.random() * 100) + 1)
+        })
+
+    }
+    
     handleOver(event){
-        console.log("My name is ", this.state.name);
+        console.log("hover me");
     }
     //JSX
     render(){
         return (
             <div>
-                My name is {this.state.name} and I'm from {this.state.address}
-                <button onClick={this.handleClick}>Clicking</button>
+                My name is {this.state.name} and I'm from {this.state.age}
+                <button onClick={(event) => {this.handleClick(event)}}>Clicking</button>
                 <button onMouseOver={this.handleOver}>Hover me</button>
             </div>
         );
