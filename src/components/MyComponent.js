d2398b5cccc12af40999ea0663c6df07aa4846b5
unknown <140576667+Dungnope@users.noreply.github.com>
2026-09-17 22:51:34 +0700

    use props with string and object beginner

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,15 +2,21 @@
 //function component
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 //viet bang class
 class MyComponent extends React.Component {
     
     //JSX
     render(){
+        const Info = ['a', 'b', 'c'];
         return (
             <div>
                 <UserInfor />
+                <br/> <br/>
+                <DisplayInfor name="Musk" age="30"/>
+                <hr/>
+                <DisplayInfor name={"Google"} age={50} myInfor = {Info}/>
             </div>
         );
     }
