4e9b412f83b66f6ac920a8db2d66ceb51fefb0d2
unknown <140576667+Dungnope@users.noreply.github.com>
2026-09-18 15:32:43 +0700

    use condition voi react

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,25 +1,45 @@
 import React from "react";
 
 class DisplayInfor extends React.Component{
- render(){
-    //destructuring array/object
-    const {listUser} = this.props;
-    console.log(listUser);
-    //props -> viet tat properties
-    return (
-        <div>
-            {listUser.map((user) => {
-                return(
-                    <div key={user.id}>
-                        <div>My name is {user.name}</div>
-                        <div>My age is {user.age}</div>
-                        <hr />
-                    </div>
-                )
-            })}
-        </div>
-    )
- }
+
+    state = {
+        isShowListUser: true
+    }
+
+
+    handleShowHide = () => {
+       this.setState({
+            isShowListUser: !this.state.isShowListUser
+       })
+    }
+
+    render(){
+       //destructuring array/object
+       const {listUser} = this.props;
+       // console.log(listUser);
+       //props -> viet tat properties
+       return (
+           <div>
+               <div>
+                   <span onClick={(event) => {this.handleShowHide()}}> {this.state.isShowListUser ? "Hide list users:" : "Show list users:"}</span>
+               </div>
+               {/* viet nhu duoi neu chi muon 1 dieu kien dung sai thi khong lam gi */}
+               {this.state.isShowListUser &&
+                   <div>
+                       {listUser.map((user) => {
+                           return(
+                           <div key={user.id} className={user.age > 18 ? "green" : "red"}>
+                               <div>My name is {user.name}</div>
+                               <div>My age is {user.age}</div>
+                               <hr />
+                           </div>
+                           )
+                       })}
+                   </div>
+               }
+           </div>
+       )
+    }
 }
 
 export default DisplayInfor
