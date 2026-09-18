441a2b28c4cf492a083dabe8b9a4d9841166c0cc
unknown <140576667+Dungnope@users.noreply.github.com>
2026-09-18 22:29:07 +0700

    work with images in React, never save image to public folder

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './DisplayInfor.scss';
+import logo from '../logo.svg';
 class DisplayInfor extends React.Component{
 
     state = {
@@ -20,6 +21,7 @@ class DisplayInfor extends React.Component{
        //props -> viet tat properties
        return (
            <div className="display-infor-container">
+               <img src={logo} alt="logo" />
                <div>
                    <span onClick={(event) => {this.handleShowHide()}}> {this.state.isShowListUser ? "Hide list users:" : "Show list users:"}</span>
                </div>
