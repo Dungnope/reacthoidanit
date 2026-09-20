4c30ca6edb3849f8fe04d332c25a10361dc80e4e
unknown <140576667+Dungnope@users.noreply.github.com>
2026-09-20 16:15:41 +0700

    class component and function component , stateful, statelesscomponent

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,74 +1,71 @@
 import React from "react";
 import './DisplayInfor.scss';
-class DisplayInfor extends React.Component{
-    //cach code dung OOP
-    //khong can dung do da co babel compiler
-    constructor(props){
-        super(props);
-        console.log(">> this run first 1")
-        this.state = {
-            isShowListUser: true
-        }
-    }
 
-    componentDidMount(){ //thuong dung goi api sau nay
-        console.log(">>> call me componenent did mount last session in first run");
-        setTimeout(() => {
-            document.title = "React by Nope"
-        }, 3000);
-    }
+//stateless vs stateful: khi chua co hook
+// class DisplayInfor extends React.Component{
+//     //cach code dung OOP
+//     //khong can dung do da co babel compiler
 
-    componentDidUpdate(prevProps, prevState, snapshot){
-        console.log(">>> call me when something update", this.props, prevProps);
-        //check item difference
-        if(this.props.listUser !== prevProps.listUser){
-            if(this.props.listUser.length === 5){
-                alert("full list");
+//     //template + logic js
+//     render(){
+//         console.log(">> call me render any time have change");
+//        //destructuring array/object
+//        const {listUser} = this.props;
+//        // console.log(listUser);
+//        //props -> viet tat properties
+//        return (
+//            <div className="display-infor-container">
+//                {true &&
+//                    <div>
+//                        {listUser.map((user) => {
+//                            return(
+//                            <div key={user.id} className={user.age > 18 ? "green" : "red"}>
+//                                <div>
+//                                     <div style={{color: "orange", marginBottom: "20px"}}>My name is {user.name}</div>
+//                                     <div>My age is {user.age}</div>
+//                                </div>
+//                                <div>
+//                                     <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
+//                                </div>
+//                                <hr />
+//                            </div>
+//                            )
+//                        })}
+//                    </div>
+//                }
+//            </div>
+//        )
+//     }
+// }
+
+const DisplayInfor = (props) => { //function component props tu dong truyen vao
+    console.log(">> call me render any time have change");
+    //destructuring array/object
+    const {listUser} = props;
+    // console.log(listUser);
+    //props -> viet tat properties
+    return (
+        <div className="display-infor-container">
+            {true &&
+                <div>
+                    {listUser.map((user) => {
+                        return(
+                        <div key={user.id} className={user.age > 18 ? "green" : "red"}>
+                            <div>
+                                 <div style={{color: "orange", marginBottom: "20px"}}>My name is {user.name}</div>
+                                 <div>My age is {user.age}</div>
+                            </div>
+                            <div>
+                                 <button onClick={() => props.handleDeleteUser(user.id)}>Delete</button>
+                            </div>
+                            <hr />
+                        </div>
+                        )
+                    })}
+                </div>
             }
-        }
-    }
-
-    handleShowHide = () => {
-       this.setState({
-            isShowListUser: !this.state.isShowListUser
-       })
-    }
-
-    //template + logic js
-    render(){
-        console.log(">> call me render any time have change");
-       //destructuring array/object
-       const {listUser} = this.props;
-       // console.log(listUser);
-       //props -> viet tat properties
-       return (
-           <div className="display-infor-container">
-               {/* <img src={logo} alt="logo" /> */}
-               <div>
-                   <span onClick={(event) => {this.handleShowHide()}}> {this.state.isShowListUser ? "Hide list users:" : "Show list users:"}</span>
-               </div>
-               {/* viet nhu duoi neu chi muon 1 dieu kien dung sai thi khong lam gi */}
-               {this.state.isShowListUser &&
-                   <div>
-                       {listUser.map((user) => {
-                           return(
-                           <div key={user.id} className={user.age > 18 ? "green" : "red"}>
-                               <div>
-                                    <div style={{color: "orange", marginBottom: "20px"}}>My name is {user.name}</div>
-                                    <div>My age is {user.age}</div>
-                               </div>
-                               <div>
-                                    <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
-                               </div>
-                               <hr />
-                           </div>
-                           )
-                       })}
-                   </div>
-               }
-           </div>
-       )
-    }
+        </div>
+    )
 }
 
 export default DisplayInfor
