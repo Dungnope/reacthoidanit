85dd625144a3d4c535f7a430a4376ccb0c1bcb6d
unknown <140576667+Dungnope@users.noreply.github.com>
2026-09-21 10:07:48 +0700

    useEffect Hook

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,57 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './DisplayInfor.scss';
 
-//stateless vs stateful: khi chua co hook
-// class DisplayInfor extends React.Component{
-//     //cach code dung OOP
-//     //khong can dung do da co babel compiler
 
-//     //template + logic js
-//     render(){
-//         console.log(">> call me render any time have change");
-//        //destructuring array/object
-//        const {listUser} = this.props;
-//        // console.log(listUser);
-//        //props -> viet tat properties
-//        return (
-//            <div className="display-infor-container">
-//                {true &&
-//                    <div>
-//                        {listUser.map((user) => {
-//                            return(
-//                            <div key={user.id} className={user.age > 18 ? "green" : "red"}>
-//                                <div>
-//                                     <div style={{color: "orange", marginBottom: "20px"}}>My name is {user.name}</div>
-//                                     <div>My age is {user.age}</div>
-//                                </div>
-//                                <div>
-//                                     <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
-//                                </div>
-//                                <hr />
-//                            </div>
-//                            )
-//                        })}
-//                    </div>
-//                }
-//            </div>
-//        )
-//     }
-// }
 
 //stateless component
+//stateless vs stateful: khi chua co hook
 const DisplayInfor = (props) => { //function component props tu dong truyen vao
-    console.log(">> call me render any time have change");
     const {listUser} = props; 
-
+    
     const [isShowHideListUser, setShowHideListUser] = useState(true);
     // [ten bien state, ham giup cap nhat bien state]
     //destructuring assigment
     // giong voi this.state = {isShowHideListUser: true}
-
+    
     const handleShowHideListUser = () => {
         setShowHideListUser(!isShowHideListUser); 
         // tuong duong voi this.setState({isShowHideListUser: !isShowHideListUser})
     }
+
+    console.log(">> call me render any time have change");
+
+    useEffect(() => { //nhu componenetDidMount
+        if(listUser.length === 0) alert("you deleted all the users");
+        console.log(">> call me useEffect");
+    }, [listUser]); // ngoac vuong thi chi chay 1 lan, bien muon react theo doi chinh la check xem componentDidUpdate
+    //useEffect dung bao nhieu tuy y
+
     //props -> viet tat properties
     return (
         <div className="display-infor-container">
